0bfae7ee351a5537976ab69b51e78dc4d0a4da08
ShmigabornDungiedoo <relonet95@gmail.com>
2018-11-19 15:48:12 +0000

    up to 19.11.2018

---
+++ ex9.py
@@ -0,0 +1,34 @@
+annual_sallary=float(input("What is your annual sallary?"))
+r=0.04                     #return on investment
+semi_annual_raise=0.07
+down_payment=250000
+current_savings=0
+high=10000
+low=0
+time=0
+addtime=0
+def bisection(high, low):
+    rate=(high+low)/2
+    return rate
+if current_savings< down_payment:
+    while t<=36:
+        rate=bisection/10000
+        portion_saved=annual_sallary/12*rate
+        monthly_return=current_savings*r/12
+        current_savings=current_savings+monthly_return+portion_saved
+if current_saving
+
+
+
+
+
+
+
+
+#while current_savings<=total_cost:
+
+
+
+    #portion_saved=annual_sallary/12*rate
+    #monthly_return=current_savings*r/12
+    #current_savings=current_savings+monthly_return+portion_saved
